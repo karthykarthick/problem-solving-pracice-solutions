eefb03ba172769563e68e4026ee6841efec565ff
karthykarthick <karthylock@gmail.com>
2022-07-16 16:12:38 +0530

    basic problem

---
+++ Basics/swap two var with using third var.java
@@ -0,0 +1,17 @@
+public class hello {
+    public static void main(String args[]){
+
+        int a = 10;
+        int b = 30;
+        int c;
+
+        c=a;// c = 10; a = ?
+        a=b;// a = 30; b = ?
+        b=c;// b = 10; c = ?
+        System.out.println(a );
+        System.out.println(b);
+     }
+}
+
+
+// Op: a = 30, b = 10
