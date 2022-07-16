b668809aca600d31bfa31bc728566caff186a168
karthykarthick <karthylock@gmail.com>
2022-07-16 16:22:56 +0530

    basic problem

---
+++ Basics/swap two var without using third var.java
@@ -0,0 +1,33 @@
+public class hello {
+    public static void main(String args[]){
+
+        // var intialization
+        int a = 10;
+        int b = 30;
+
+        // Logic
+        a = a + b; // a = 40;
+        b = a - b; // b = 40 -30 -> b = 10
+        a = a - b; // a = 40 - 10 -> a = 30
+        
+        //print the OP
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
