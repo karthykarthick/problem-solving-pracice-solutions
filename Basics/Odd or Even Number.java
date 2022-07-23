147bb655bf76075f8e298a2ea7edf4792af7dbcd
karthykarthick <karthylock@gmail.com>
2022-07-23 23:33:01 +0530

    odd or even number program

---
+++ Basics/Odd or Even Number.java
@@ -0,0 +1,29 @@
+public class hello {
+    public static void main(String[] args){
+
+        int a = 70;
+
+        if(a%2==0){
+            System.out.println("Even Number");
+        }else {
+            System.out.println("Odd Number");
+        }
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
