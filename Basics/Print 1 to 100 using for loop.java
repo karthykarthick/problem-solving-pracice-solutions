1ada2cb19bf6dbb6a58258c1e296109e19074f8f
karthykarthick <karthylock@gmail.com>
2022-07-23 22:55:56 +0530

    add for loop program

---
+++ Basics/Print 1 to 100 using for loop.java
@@ -0,0 +1,24 @@
+public class hello {
+    public static void main(String[] args){
+        for (int i = 1; i<=100; ++i){
+            System.out.println(i);
+        }
+     }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
