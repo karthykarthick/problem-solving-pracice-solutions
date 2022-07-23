f1340bec6ac9b895051fda3b5f6cdca0616f7f67
karthykarthick <karthylock@gmail.com>
2022-07-23 23:29:08 +0530

    Biggest of 3 numbers

---
+++ Basics/Biggest of 3 numbers.java
@@ -0,0 +1,33 @@
+public class hello {
+    public static void main(String[] args){
+
+        int  a = 800;
+        int  b = 400;
+        int  c = 300;
+
+        if ((a>b) && (a>c)){
+            System.out.println(a);
+        } else if ((b>a) && (b>c)) {
+            System.out.println(b);
+        }else {
+            System.out.println(c);
+        }
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
