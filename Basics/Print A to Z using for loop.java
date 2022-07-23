bd3a20d8e853917c619ac3663b5920724bddc1a8
karthykarthick <karthylock@gmail.com>
2022-07-23 23:07:11 +0530

    added a to z for loop program

---
+++ Basics/Print A to Z using for loop.java
@@ -0,0 +1,24 @@
+public class hello {
+    public static void main(String[] args){
+        for (char i = 65; i<=90; ++i){
+            System.out.println(i);
+        }
+     }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
