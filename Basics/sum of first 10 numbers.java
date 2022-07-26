4f39455700ee57dbc6c5c98a0e2f9cef16985dbb
karthykarthick <karthylock@gmail.com>
2022-07-26 14:27:16 +0530

    sum of first numbers

---
+++ Basics/sum of first 10 numbers.java
@@ -0,0 +1,14 @@
+public class hello {
+    public static void main(String[] args){
+
+        int sum = 0;
+        for (int i = 1; i<=10; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+
+
