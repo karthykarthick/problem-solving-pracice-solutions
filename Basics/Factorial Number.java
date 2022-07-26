46f7de93a99b84e1c5cc239d45888e37eb2a810b
karthykarthick <karthylock@gmail.com>
2022-07-26 18:06:59 +0530

    Factorial Number

---
+++ Basics/Factorial Number.java
@@ -0,0 +1,23 @@
+public class hello {
+    public static void main(String[] args){
+
+
+        int fact = 5;
+        int val = 1;
+		
+        for (int i = fact; 1 <= i; i--){
+            val = val * i;  // 5*4*3*2*1
+        }
+        System.out.println(val);
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+
+
+
+
+
+
+
+
