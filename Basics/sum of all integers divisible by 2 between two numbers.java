38d2c9ba2207b9b069730c44c7374ef565eec802
karthykarthick <karthylock@gmail.com>
2022-07-26 17:49:59 +0530

    odd or even number program

---
+++ Basics/sum of all integers divisible by 2 between two numbers.java
@@ -0,0 +1,25 @@
+public class hello {
+    public static void main(String[] args){
+
+        int num_one = 1;
+        int num_two = 10;
+        int sum = 0;
+        
+        for(int i = num_one; i<= num_two; i++){
+          if(i % 2 == 0){
+              sum = sum + i;
+          }
+        }
+        System.out.println(sum);
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+// sum of all integers divisible by 2 between two numbers
+
+
+
+
+
+
+
