e1c2e51888a9ba4b84f37d50d007d08b6203e0dd
karthykarthick <karthylock@gmail.com>
2022-07-26 17:39:13 +0530

    reverse a number

--- Basics/palindrome_Number.java
+++ Basics/palindrome_Number.java
@@ -1,12 +1,12 @@
 public class hello {
     public static void main(String[] args){
 
-        String str = "123";
-        int len = str.length();
-        int count = 0;
+        String str = "123"; 
+        int len = str.length();  // store the string len in s variable
+        int count = 0;           // to count the number of char are same so we can compare with length of original string
 //      System.out.println(str.length());
         for (int i = 0;i<=str.length()-1;i++){
-            if ((str.charAt(i) == str.charAt(len-1-i))){
+            if ((str.charAt(i) == str.charAt(len-1-i))){ 
                 count++;
             }
         }
@@ -20,7 +20,9 @@ public class hello {
 
 // Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
 
-
+// if ((str.charAt(i) == str.charAt(len-1-i)))   //Here we are checking like
+// 1 == 3 
+// 2 == 2 like this we can check with the input!
 
 
 
