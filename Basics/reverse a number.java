11e1f13e683354f0a99bf0e1fa8f6e4cdbb3a823
karthykarthick <karthylock@gmail.com>
2022-07-26 16:13:08 +0530

    reverse a number

---
+++ Basics/reverse a number.java
@@ -0,0 +1,29 @@
+public class hello {
+    public static void main(String[] args){
+
+     int num = 1234;
+     int rev = 0;
+     while (num!=0){
+        int lastDigit = num % 10;
+        rev = (rev*10)+lastDigit;
+        num = num / 10;
+     }
+        System.out.println(rev);
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+// while loop ---> HERE WE USE while loop in for we cant initiate the I so i go with while looop
+
+
+
+
+
+
+
+
+
+
+
+
+
