0d485747aefe1a94cf2ca88943b7da0bd4c7d6b9
karthykarthick <karthylock@gmail.com>
2022-07-27 17:07:48 +0530

    star pattern 1 and its reverse

---
+++ Basics/star pattern - 1 - reverse.java
@@ -0,0 +1,29 @@
+import static java.lang.Math.pow;
+
+public class hello {
+    public static void main(String[] args){
+
+      for (int i = 5; i>=1; i--){
+          for (int j = 1; j<=i; j++){
+              System.out.print("*");
+          }
+          System.out.println("");
+      }
+
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+
+
+op:
+*****
+****
+***
+**
+*
+
+
+
+
+
---
+++ Basics/star pattern - 1.java
@@ -0,0 +1,22 @@
+import static java.lang.Math.pow;
+
+public class hello {
+    public static void main(String[] args){
+        for(int i = 1; i<=5; i++){
+            for (int j = 1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+
+
+
+
+
+
+
+
