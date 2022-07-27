ad476ad88799312b9b4434b3b755759d02593380
karthykarthick <karthylock@gmail.com>
2022-07-27 17:09:20 +0530

    star pattern 1 and reverse combined

---
+++ Basics/star pattern - 1 - reverse - combined.java
@@ -0,0 +1,39 @@
+import static java.lang.Math.pow;
+
+public class hello {
+    public static void main(String[] args){
+
+        for (int i = 1; i<=5;i++){
+            for (int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for (int i = 5; i>=1; i--){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+
+op:
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+//        *****
+//        ****
+//        ***
+//        **
+//        *
+
+
+
