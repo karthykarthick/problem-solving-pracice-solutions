80ea0fb2880995d58ae59c68aa83900e0ae20aca
karthykarthick <karthylock@gmail.com>
2022-07-29 18:47:17 +0530

    number pattern - 1 - reverse

---
+++ Basics/number pattern - 1 - reverse.java
@@ -0,0 +1,21 @@
+public class hello {
+    public static void main(String[] args){
+
+
+        for(int i = 5; i>=1; i--){
+            for (int j = 1; i>=j; j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+// op:
+//        12345
+//        1234
+//        123
+//        12
+//        1
