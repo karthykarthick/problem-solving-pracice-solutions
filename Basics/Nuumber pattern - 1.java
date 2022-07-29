0ce307cefb0392880241d9e773b755ae21ed9ed3
karthykarthick <karthylock@gmail.com>
2022-07-29 18:43:18 +0530

    number pattern-1

---
+++ Basics/Nuumber pattern - 1.java
@@ -0,0 +1,20 @@
+
+public class hello {
+    public static void main(String[] args){
+
+        int num = 5;
+        for(int i=1; i<=num;i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
+
+// Re-visit pre-post increment and greater than and less than symbol,Modulus Operator
+//1
+//12
+//123
+//1234
+//12345
